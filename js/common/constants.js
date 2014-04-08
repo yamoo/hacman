739d9e8b7e8f9bce811166ce861b6d4a67e7aa25
shinsuke.a.yamada <shinsuke.a.yamada@mail.rakuten.com>
2014-04-08 08:56:25 +0100

    changed env

--- js/common/constants.js
+++ js/common/constants.js
@@ -2,8 +2,8 @@ HAC.define('Const',[
 ], function() {
 
 	return {
-		server: 'http://localhost:33000',
-		//server: 'http://VLB12-28:33000',
+		//server: 'http://localhost:33000',
+		server: 'http://VLB12-28:3000',
 		storage: 'hacman_data_v0.1',
 		timeout: 30000,
 		world: {
